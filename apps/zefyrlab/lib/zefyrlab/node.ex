338a98210e604bc7d84b0645f575468903bac000
zefiro <gitlab.pushy576@passmail.net>
2025-12-14 14:36:02 +0100

    chore: rm stormforge

--- apps/zefyrlab/lib/zefyrlab/node.ex
+++ apps/zefyrlab/lib/zefyrlab/node.ex
@@ -1,10 +1,10 @@
 defmodule Zefyrlab.Node do
   @moduledoc """
-  Thorchain node client for Stormforge application.
+  Thorchain node client for Zefyrlab application.
 
   Uses Cosmos.Node to provide gRPC connectivity to Thorchain nodes
   with separate pools for queries and transactions.
   """
 
-  use Cosmos.Node, otp_app: :stormforge, publisher: Zefyrlab.Publisher
+  use Cosmos.Node, otp_app: :zefyrlab, publisher: Zefyrlab.Publisher
 end
