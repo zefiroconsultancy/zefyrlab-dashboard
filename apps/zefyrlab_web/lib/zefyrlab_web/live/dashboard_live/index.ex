28e649354ab1df12122e7b9dba9400333f806436
zefiro <gitlab.pushy576@passmail.net>
2025-12-14 14:48:26 +0100

    chore: test logout

--- apps/zefyrlab_web/lib/zefyrlab_web/live/dashboard_live/index.ex
+++ apps/zefyrlab_web/lib/zefyrlab_web/live/dashboard_live/index.ex
@@ -24,6 +24,14 @@ defmodule ZefyrlabWeb.DashboardLive.Index do
         <h2 class="text-xl font-semibold mb-4">Welcome!</h2>
         <p>Your authenticated dashboard is ready.</p>
       </div>
+
+            <.link
+        href={~p"/auth/log_out"}
+        method="delete"
+        class="ml-auto inline-flex items-center rounded-md bg-zinc-900 px-3 py-2 text-sm font-semibold text-white hover:bg-zinc-700"
+      >
+        Log out
+      </.link>
     </div>
     """
   end
